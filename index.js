4b75efcd228db67bf418cf325ddb957ace2e6abc
pmlein <pmlein@users.noreply.github.com>
2018-12-13 13:55:05 +0200

    cors

--- index.js
+++ index.js
@@ -154,6 +154,14 @@ app.post('/api/persons', (request, response) => {
 })
 
 
+const error = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(error)
+
+
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
